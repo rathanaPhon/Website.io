c6db33564e7eb14e85afbc27ee563bf647e7946d
Rathana.github.io <153509354+rathanaPhon@users.noreply.github.com>
2024-02-20 04:36:35 +0700

    Initial commit

---
+++ JavaScript/index.js
@@ -0,0 +1,18 @@
+ <script src="https://rathanaphon.github.io/wedpage/"></script> 
+ function onlick(num) {
+     $(document).ready(function(num){
+         console.log("if");
+            $.ajax({
+              dataType: "jsonp",
+              type: 'GET',
+              url: "https://rathanaphon.github.io/wedpage./">
+              function onsigin(data) {
+              console.log("Gmail");
+           },
+              error: function(num){
+              console.log("message"); 0,1,2,3
+            }
+         });
+    });
+}
+    
