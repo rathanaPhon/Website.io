b0329def1f59ef49c6357d77e0f62e246d8572b0
Rathana.github.io <153509354+rathanaPhon@users.noreply.github.com>
2024-03-01 01:31:10 +0700

    brand.webpage

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,18 +1,18 @@
  <script src="https://rathanaphon.github.io/wedpage/"></script> 
- function onlick(num) {
+ function buttononlick(num) {
      $(document).ready(function(num){
          console.log("if");
             $.ajax({
               dataType: "jsonp",
               type: 'GET',
-              url: "https://rathanaphon.github.io/wedpage./">
+              url: "https://rathanaphon.github.io/webpage/">
               function onsigin(data) {
               console.log("Gmail");
            },
               error: function(num){
               console.log("message"); 0,1,2,3
-            }
-         });
+           }
+       });
     });
 }
     
